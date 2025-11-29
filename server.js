ddf68eafd34e9dede584845f5321135deeac44bd
9382RAHUL <rahulmodak492@gmail.com>
2025-11-30 02:32:00 +0530

    chann

--- server.js
+++ server.js
@@ -43,7 +43,7 @@
 
 // require("dotenv").config()
 // server.js
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
